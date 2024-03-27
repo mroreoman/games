0996a4b90504278020e1ca750af354fa18f9ee90
aryan singh <aryan.singh@motsd.org>
2024-03-26 22:50:50 -0400

    made java ageguess

---
+++ java/AgeGuess.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class AgeGuess {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+    
+        System.out.println("guess my age!");
+        int age = rand.nextInt(100);
+        int guess = -1;
+        int guesses = 0;
+
+        while (guess != age) {
+            guesses++;
+            System.out.print("guess: ");
+            guess = scan.nextInt();
+            if (guess > age) {
+                System.out.println("too high!");
+            } else if (guess < age) {
+                System.out.println("too low!");
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("you got it! it took " + guesses + " tries!");
+    }
+}
