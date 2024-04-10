f969fdaaba236b0891ca9c266081d3fcfb57c0a8
mroreoman <830aryans@gmail.com>
2024-04-10 16:50:00 -0400

    grrrraah

--- java/Wordle.java
+++ java/Wordle.java
@@ -1,14 +1,30 @@
 import java.io.File;
-import java.util.stream.Stream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
+import java.util.Scanner;
 
 public class Wordle {
-    public static void main(String[] args) throws Exception {
-        Path cur = new File("").toPath().toAbsolutePath();
-        Stream<String> wordleLa = Files.lines(cur.resolve("wordleDictionary/wordle-Ta.txt"));
-        Stream<String> wordleTa = Files.lines(cur.resolve("wordleDictionary/wordle-Ta.txt"));
+    static Scanner scan = new Scanner(System.in);
+    static Stream<String> wordleLa, wordleTa;
 
-        wordleTa.forEach(s -> System.out.println(s));
-    }   
+    public static void main(String[] args) throws Exception { //TODO: reformat all the games to work with a runner
+        Path cur = new File("").toPath().toAbsolutePath();
+        wordleLa = Files.lines(cur.resolve("wordleDictionary/wordle-Ta.txt"));
+        wordleTa = Files.lines(cur.resolve("wordleDictionary/wordle-Ta.txt"));
+        
+        wordleLa.skip((int)(Math.random() * wordleLa.count()));
+        String word = wordleLa.findFirst().orElse(null);
+        int guesses = 0;
+        String guess = "";
+
+        while (guess != word) {
+            guess = input();
+
+        }
+    }
+
+    static String input() {
+        return scan.nextLine();
+    }
 }
