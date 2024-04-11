afbe9c7866c50f26ce18e6ed150afda088967b2f
mroreoman <830aryans@gmail.com>
2024-04-11 14:05:14 -0400

    minesweeper improved + uses game interface

--- java/Minesweeper.java
+++ java/Minesweeper.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Minesweeper {
-    enum Marks {HIDDEN, FLAGGED, REVEALED};
+public class Minesweeper implements Game {
+    private static enum Mark {HIDDEN, FLAGGED, REVEALED};
 
-    enum Colors {
+    private static enum Colors {
         WHITE("\u001B[37m"),
         BLUE("\u001B[34m"),
         GREEN("\u001B[32m"),
@@ -28,82 +28,18 @@ public class Minesweeper {
         }
     }
 
-    static Scanner scan = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        Board b = new Board(9,9,10);
-        System.out.println("\n" + b);
-        play(b);
-    }
-
-    private static void play(Board board) {
-        while (true) {
-            System.out.print("move: ");
-            String move = scan.nextLine().toLowerCase();
-
-            if (move.equals("x"))
-                return;
-
-            int x, y;
-            while (true) {
-                System.out.print("x,y: ");
-                String[] raw = scan.nextLine().split(",");
-                try {
-                    x = Integer.parseInt(raw[0]);
-                    y = Integer.parseInt(raw[1]);
-                } catch (NumberFormatException e) {
-                    continue;
-                }
-                break;
-            }
-            
-            
-            
-            switch (move) {
-                case "c": {
-                    if (board.clickTile(x, y)) {
-                        System.out.println("you lost brah");
-                        return;
-                    }
-                    break;
-                } case "f": {
-                    board.flagTile(x, y, true);
-                    break;
-                } case "u": {
-                    board.flagTile(x, y, false);
-                    break;
-                } case "cn": {
-                    if (board.clickNeighbors(x, y)) {
-                        System.out.println("you lost brah");
-                        return;
-                    }
-                    break;
-                } case "fn": {
-                    board.flagNeighbors(x, y, true);
-                    break;
-                } case "un": {
-                    board.flagNeighbors(x, y, false);
-                    break;
-                } default:
-                    continue;
-            }
-            
-            System.out.println(board);
-        }
-    }
-
     private static class Tile {
         private boolean isMine = false;
         private int neighbors;
-        private Marks mark = Marks.HIDDEN;
+        private Mark mark = Mark.HIDDEN;
 
         public String toString() {
             switch (mark) {
-                case Marks.HIDDEN:
+                case Mark.HIDDEN:
                     return "_";
-                case Marks.FLAGGED:
-                    return "F";
-                case Marks.REVEALED:
+                case Mark.FLAGGED:
+                    return Colors.RED + "F" + Colors.RESET;
+                case Mark.REVEALED:
                     if (isMine) {
                         return Colors.RED + "x" + Colors.RESET;
                     } else {
@@ -113,145 +49,249 @@ public class Minesweeper {
                     return "_";
             }
         }
+
+        public boolean isCorrect() {
+            if (isMine) {
+                return mark != Mark.REVEALED;
+            } else {
+                return mark == Mark.REVEALED;
+            }
+        }
     }
 
-    private static class Board {
-        private Tile[][] tiles;
-        private ArrayList<Tile> list;
-        private int mines;
+    private final static Scanner scan = new Scanner(System.in);
+    private final static Random rand = new Random();
 
-        public Board(int width, int height, int mines) {
-            tiles = new Tile[height][width];
-            list = new ArrayList<Tile>();
-            this.mines = mines;
+    private Tile[][] board;
+    private int mines;
+    private boolean alive = true;
+    private int moves = 0;
+    private int flags = 0; //TODO this is not accurate
 
-            for (int y = 0; y < tiles.length; y++) {
-                for (int x = 0; x < tiles[y].length; x++) {
-                    tiles[y][x] = new Tile();
-                    list.add(tiles[y][x]);
-                }
+    public Minesweeper() {
+        board = new Tile[9][9];
+        mines = 10;
+        ArrayList<Tile> list = new ArrayList<Tile>();
+
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[y].length; x++) {
+                board[y][x] = new Tile();
+                list.add(board[y][x]);
+            }
+        }
+        
+        System.out.println(sBoard());
+        System.out.println("first click: ");
+        int[] coords = scanClick();
+        list.remove(board[coords[1]][coords[0]]);
+
+        for (int i = 0; i < mines; i++) {
+            int n = rand.nextInt(list.size());
+            list.get(n).isMine = true;
+            list.remove(n);
+        }
+
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[y].length; x++) {
+                board[y][x].neighbors = countNeighbors(x, y);
             }
         }
 
-        public void flagTile(int x, int y, boolean flag) {
+        clickTile(coords[0], coords[1]);
+        moves++;
+        System.out.println(sBoard());
+    }
+
+    public State play() {
+        System.out.print("move: ");
+        String move = scan.nextLine().toLowerCase();
+
+        if (move.equals("x"))
+            return State.PLAYING;
+
+        int[] coords = scanClick();
+        int x = coords[0];
+        int y = coords[1];
+        State s = State.PLAYING;
+        Mark old = board[y][x].mark;
+        
+        switch (move) {
+            case "c": {
+                s = clickTile(x, y);
+                break;
+            } case "f": {
+                flagTile(x, y, true);
+                break;
+            } case "u": {
+                flagTile(x, y, false);
+                break;
+            } case "cn": {
+                s = clickNeighbors(x, y);
+                break;
+            } case "fn": {
+                flagNeighbors(x, y, true);
+                break;
+            } case "un": {
+                flagNeighbors(x, y, false);
+                break;
+            }
+        }
+
+        if (board[y][x].mark != old)
+            moves++;
+        
+        System.out.println(sBoard());
+        return s;
+    }
+
+    public String score() {
+        if (won()) {
+            return "you won! it took " + moves + " moves.";
+        } else {
+            return "you lost brah";
+        }
+    }
+
+    /**returns array of {x,y} */
+    private int[] scanClick() {
+        int x, y;
+        while (true) {
+            System.out.print("x,y: ");
+            String[] raw = scan.nextLine().split(",");
+            try {
+                x = Integer.parseInt(raw[0].trim());
+                y = Integer.parseInt(raw[1].trim());
+            } catch (Exception e) {
+                continue;
+            }
             if (!tileInBounds(x, y))
-                return;
-            if (tiles[y][x].mark == Marks.REVEALED)
-                return;
-            
-            tiles[y][x].mark = flag ? Marks.FLAGGED : Marks.HIDDEN;
+                continue;
+            break;
         }
 
-        // returns true if u die
-        public boolean clickTile(int x, int y) {
-            if (!tileInBounds(x, y))
-                return false;
+        return new int[]{x, y};
+    }
+
+    private int countNeighbors(int x, int y) {
+        int neighbors = 0;
+        for (int yy = y-1; yy <= y+1; yy++) {
+            for (int xx = x-1; xx <= x+1; xx++) {
+                if (yy == y && xx == x)
+                    continue;
+                if (!tileInBounds(xx, yy))
+                    continue;
+                if (board[yy][xx].isMine)
+                    neighbors++;
+            }
+        }
+        return neighbors;
+    }
+
+    public void flagTile(int x, int y, boolean flag) {
+        if (!tileInBounds(x, y))
+            return;
+        if (board[y][x].mark == Mark.REVEALED)
+            return;
+        
+        if (flag) {
+            board[y][x].mark = Mark.FLAGGED;
+            flags++;
+        } else {
+            board[y][x].mark = Mark.HIDDEN;
+            flags--;
+        }
+    }
+
+    public State clickTile(int x, int y) {
+        if (!tileInBounds(x, y))
+            return State.PLAYING;
+        
+        if (board[y][x].mark != Mark.HIDDEN)
+            return State.PLAYING;
+        
+        board[y][x].mark = Mark.REVEALED;
+        if (board[y][x].isMine)
+            return State.LOST;
+        
+        if (board[y][x].neighbors == 0)
+            clickNeighbors(x, y);
+        
+        if (won())
+            return State.WON;
+        
+        return State.PLAYING;
+    }
+
+    public void flagNeighbors(int x, int y, boolean flag) {
+        if (board[y][x].mark != Mark.REVEALED)
+            return;
+
+        for (int yy = y-1; yy <= y+1; yy++) {
+            for (int xx = x-1; xx <= x+1; xx++) {
+                if (yy == y && xx == x)
+                    continue;
                 
-            // sets mines after first click to avoid instant death
-            if (list.size() == tiles.length * tiles[0].length)
-                setMines(tiles[y][x], mines);
-            
-            if (tiles[y][x].mark != Marks.HIDDEN)
-                return false;
+                flagTile(xx, yy, flag);
+            }
+        }
+    }
+
+    public State clickNeighbors(int x, int y) {
+        if (board[y][x].mark != Mark.REVEALED)
+            return State.PLAYING;
+
+        for (int yy = y-1; yy <= y+1; yy++) {
+            for (int xx = x-1; xx <= x+1; xx++) {
+                if (yy == y && xx == x)
+                    continue;
                 
-            tiles[y][x].mark = Marks.REVEALED;
-            if (tiles[y][x].isMine)
-                return true;
-            
-            if (tiles[y][x].neighbors == 0)
-                clickNeighbors(x, y);
-            
+                State s = clickTile(xx, yy);
+                if (s != State.PLAYING)
+                    return s;
+            }
+        }
+
+        return State.PLAYING;
+    }
+
+    public boolean won() {
+        if (!alive)
             return false;
-        }
+        
+        for (Tile[] row : board)
+            for (Tile tile : row)
+                if (!tile.isCorrect())
+                    return false;
 
-        public void flagNeighbors(int x, int y, boolean flag) {
-            if (tiles[y][x].mark != Marks.REVEALED)
-                return;
+        return true;
+    }
 
-            for (int yy = y-1; yy <= y+1; yy++) {
-                for (int xx = x-1; xx <= x+1; xx++) {
-                    if (yy == y && xx == x)
-                        continue;
-                    
-                    flagTile(xx, yy, flag);
-                }
-            }
-        }
-
-        public boolean clickNeighbors(int x, int y) {
-            if (tiles[y][x].mark != Marks.REVEALED)
-                return false;
-
-            for (int yy = y-1; yy <= y+1; yy++) {
-                for (int xx = x-1; xx <= x+1; xx++) {
-                    if (yy == y && xx == x)
-                        continue;
-                    
-                    if (clickTile(xx, yy))
-                        return true;
-                }
-            }
-
+    private boolean tileInBounds(int x, int y) {
+        if (y < 0 || y > board.length - 1)
             return false;
+        if (x < 0 || x > board[y].length - 1)
+            return false;
+        return true;
+    }
+
+    public String sBoard() {
+        String s = "mines: " + (mines - flags) + "\t" + (alive ? ":)" : ":(") + "\tmoves: " + moves + "\n\n";
+        s += "\t";
+        for (int i = 0; i < board[0].length; i++) {
+            s += i + " ";
         }
-
-        private void setMines(Tile clicked, int mines) {
-            Random rand = new Random();
-
-            list.remove(clicked);
-
-            for (int i = 0; i < mines; i++) {
-                int n = rand.nextInt(list.size());
-                list.get(n).isMine = true;
-                list.remove(n);
-            }
-
-            setNeighbors();
-        }
-
-        private void setNeighbors() {
-            for (int y = 0; y < tiles.length; y++) {
-                for (int x = 0; x < tiles[y].length; x++) {
-                    int neighbors = 0;
-
-                    for (int yy = y-1; yy <= y+1; yy++) {
-                        for (int xx = x-1; xx <= x+1; xx++) {
-                            if (yy == y && xx == x)
-                                continue;
-                            if (!tileInBounds(xx, yy))
-                                continue;
-                            if (tiles[yy][xx].isMine)
-                                neighbors++;
-                        }
-                    }
-
-                    tiles[y][x].neighbors = neighbors;
+        s += "\n\n";
+        for (int y = 0; y < board.length; y++) {
+            s += y + "\t";
+            for (int x = 0; x < board[y].length; x++) {
+                if (board[y][x] == null) {
+                    s += "_" + " ";
+                } else {
+                    s += board[y][x] + " ";
                 }
-            }
-        }
-
-        private boolean tileInBounds(int x, int y) {
-            if (y < 0 || y > tiles.length - 1)
-                return false;
-            if (x < 0 || x > tiles[y].length - 1)
-                return false;
-            return true;
-        }
-
-        public String toString() {
-            String s = "  ";
-            for (int i = 0; i < tiles[0].length; i++) {
-                s += i + " ";
             }
             s += "\n";
-            for (int y = 0; y < tiles.length; y++) {
-                s += y + " ";
-                for (int x = 0; x < tiles[y].length; x++) {
-                    s += tiles[y][x] + " ";
-                }
-                s += "\n";
-            }
-            return s;
         }
+        return s;
     }
 }
