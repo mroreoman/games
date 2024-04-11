5714b3458c3c8cc353ee9ad1f582829e6d61ad51
mroreoman <830aryans@gmail.com>
2024-04-10 21:57:17 -0400

    input validation

--- java/Wordle.java
+++ java/Wordle.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Wordle {
@@ -15,16 +16,41 @@ public class Wordle {
         
         wordleLa.skip((int)(Math.random() * wordleLa.count()));
         String word = wordleLa.findFirst().orElse(null);
+        HashMap<Character, Integer> letterCount = new HashMap<>();
+
         int guesses = 0;
         String guess = "";
 
         while (guess != word) {
             guess = input();
-
+            if (guess == null)
+                continue;
+            
         }
     }
 
     static String input() {
-        return scan.nextLine();
+        String in = scan.nextLine();
+        in.toLowerCase();
+        if (in.length() != 5) {
+            System.out.println("input is incorrect length");
+        } else if (!isAlpha(in)) {
+            System.out.println("guess must be letters");
+        } else if (wordleLa.anyMatch(s -> s.equals(in)) || wordleTa.anyMatch(s -> s.equals(in))) {
+            System.out.println("guess is not in word list");
+        } else {
+            return in;
+        }
+        return null;
+    }
+
+    static boolean isAlpha(String s) {
+        char[] chars = s.toCharArray();
+
+        for (char c : chars)
+            if (!Character.isLetter(c))
+                return false;
+        
+        return true;
     }
 }
