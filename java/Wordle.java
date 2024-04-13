f93ec292fa7fb3bb0d419b146830ee4c74243da3
mroreoman <830aryans@gmail.com>
2024-04-13 15:02:23 -0400

    finished wordle

--- java/Wordle.java
+++ java/Wordle.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -14,51 +14,25 @@ public class Wordle implements Game {
 
     private String word;
     private int guesses = 0;
-    private HashMap<Character, Integer> _charCounts;
 
     public Wordle() throws IOException {
         Path cur = new File("").toPath().toAbsolutePath();
-        wordleLa = Files.readAllLines(cur.resolve("wordleDictionary/wordle-Ta.txt"));
+        wordleLa = Files.readAllLines(cur.resolve("wordleDictionary/wordle-La.txt"));
         wordleTa = Files.readAllLines(cur.resolve("wordleDictionary/wordle-Ta.txt"));
         
         word = wordleLa.get(rand.nextInt(wordleLa.size()));
-        _charCounts = new HashMap<>();
-        for (char c : word.toCharArray()) {
-            if (_charCounts.containsKey(c)) {
-                _charCounts.put(c, _charCounts.get(c) + 1);
-            } else {
-                _charCounts.put(c, 1);
-            }
-        }
     }
 
     public State play() {
         String guess = input();
-
-        if (guess == null) {
-            return State.PLAYING;
-        }
+        guesses++;
         
         if (guess.equals(word)) {
             System.out.println(Colors.GREEN + guess + Colors.RESET);
             return State.WON;
         }
         
-        HashMap<Character, Integer> charCounts = (HashMap) _charCounts.clone();
-        for (int i = 0; i < guess.length(); i++) {
-            char c = guess.toCharArray()[i];
-            if (c == word.toCharArray()[i]) {
-                System.out.print(Colors.GREEN + String.valueOf(c) + Colors.RESET);
-                charCounts.put(c, charCounts.get(c) - 1);
-            } else if (charCounts.get(c) > 0) {
-                System.out.print(Colors.YELLOW + String.valueOf(c) + Colors.RESET);
-                charCounts.put(c, charCounts.get(c) - 1);
-            } else {
-                System.out.print(Colors.RED + String.valueOf(c) + Colors.RESET);
-            }
-        }
-        
-        guesses++;
+        System.out.println(checkGuess(guess));
         return State.PLAYING;
     }
 
@@ -66,27 +40,52 @@ public class Wordle implements Game {
         return "guesses: " + guesses;
     }
 
+    private String checkGuess(String guess) {
+        ArrayList<Character> letters = new ArrayList<>(5);
+        for (char c : word.toCharArray()) {
+            letters.add(c);
+        }
+
+        int i = 0;
+        for (char c : guess.toCharArray()) {
+            if (c == word.charAt(i++)) {
+                letters.remove(Character.valueOf(c));
+            }
+        }
+        
+        String s = "";
+        for (i = 0; i < word.length(); i++) {
+            char c = guess.charAt(i);
+            if (c == word.charAt(i)) {
+                s += Colors.GREEN + String.valueOf(c) + Colors.RESET;
+            } else if (letters.remove(Character.valueOf(c))) {
+                s += Colors.YELLOW + String.valueOf(c) + Colors.RESET;
+            } else {
+                s += Colors.RED + String.valueOf(c) + Colors.RESET;
+            }
+        }
+        return s;
+    }
 
     private static String input() {
-        System.out.print("guess: ");
-        String in = scan.nextLine();
-        in.toLowerCase();
-        if (in.length() != 5) {
-            System.out.println("input is incorrect length");
-        } else if (!isAlpha(in)) {
-            System.out.println("guess must be letters");
-        } else if (!wordleLa.contains(in) && !wordleTa.contains(in)) {
-            System.out.println("guess is not in word list"); //TODO crane not in word list
-        } else {
-            return in;
+        while (true) {
+            System.out.print("guess: ");
+            String in = scan.nextLine();
+            in.toLowerCase();
+            if (in.length() != 5) {
+                System.out.println("input is incorrect length");
+            } else if (!isAlpha(in)) {
+                System.out.println("guess must be letters");
+            } else if (!wordleLa.contains(in) && !wordleTa.contains(in)) {
+                System.out.println("guess is not in word list");
+            } else {
+                return in;
+            }
         }
-        return null;
     }
 
     static boolean isAlpha(String s) {
-        char[] chars = s.toCharArray();
-
-        for (char c : chars)
+        for (char c : s.toCharArray())
             if (!Character.isLetter(c))
                 return false;
         
